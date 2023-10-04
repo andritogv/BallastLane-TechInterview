3b01823f4a8ba64b2f6de975d38bea399dd1423f
Alex Gonzalez <andritogv@gmail.com>
2023-10-03 23:07:51 -0600

    Added test for Prescription

---
+++ src/PrescriberPoint.Domain.Tests/PrescriptionTests.cs
@@ -0,0 +1,26 @@
+﻿namespace PrescriberPoint.Domain.Tests;
+
+public class PrescriptionTests
+{
+    [Fact]
+    public void TestGetSetProperties()
+    {
+        const int id = 1;
+        const string name = "testprescriptionname";
+        const string description = "testdescriptiondescription";
+        const int userId = 1;
+
+        var prescription = new Prescription
+        {
+            Id = id,
+            Name = name,
+            Description = description,
+            UserId = userId
+        };
+
+        Assert.Equal(id, prescription.Id);
+        Assert.Equal(name, prescription.Name);
+        Assert.Equal(description, prescription.Description);
+        Assert.Equal(userId, prescription.UserId);
+    }
+}
