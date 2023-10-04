35bb138b9c77d1b5fc5e6470ab00300c90e72981
Alex Gonzalez <andritogv@gmail.com>
2023-10-03 23:01:47 -0600

    Added code to pass first test

--- src/PrescriberPoint.Domain/User.cs
+++ src/PrescriberPoint.Domain/User.cs
@@ -1,6 +1,7 @@
 ﻿namespace PrescriberPoint.Domain;
 
-public class UserTests
+public class User
 {
-    
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
 }
