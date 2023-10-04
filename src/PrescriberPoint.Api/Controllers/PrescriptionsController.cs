17dc48267cf79bef17801ca173bc608253176d42
Alex Gonzalez <andritogv@gmail.com>
2023-10-04 00:25:20 -0600

    Added Tests project for API

---
+++ src/PrescriberPoint.Api/Controllers/PrescriptionsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrescriberPoint.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrescriptionsController : ControllerBase
+    {
+        // GET: api/<PrescriptionsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PrescriptionsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PrescriptionsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PrescriptionsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PrescriptionsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
