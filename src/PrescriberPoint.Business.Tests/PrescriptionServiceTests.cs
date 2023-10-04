81f3b4be6f7e5da3a9769706102f6bc85cce3374
Alex Gonzalez <andritogv@gmail.com>
2023-10-03 23:50:13 -0600

    Updated nuget packages and added Moq

--- src/PrescriberPoint.Business.Tests/PrescriptionServiceTests.cs
+++ src/PrescriberPoint.Business.Tests/PrescriptionServiceTests.cs
@@ -1,22 +1,20 @@
-﻿using PrescriberPoint.Business.Prescriptions;
+﻿using Moq;
+using PrescriberPoint.Business.Prescriptions;
+using PrescriberPoint.Business.Repositories;
 using PrescriberPoint.Domain;
 
 namespace PrescriberPoint.Business.Tests;
 
 public class PrescriptionServiceTests
 {
-    private readonly IPrescriptionService _sut;
-
-    public PrescriptionServiceTests()
-    {
-        _sut = new PrescriptionService();
-    }
-
     [Fact]
     public void TestAddPrescriptionShouldContainData()
     {
+        var prescriptionRepositoryMock = new Mock<IPrescriptionRepository>();
+        var sut = new PrescriptionService(prescriptionRepositoryMock.Object);
+
         var prescription = new Prescription();
 
-        Assert.Throws<ArgumentException>(() => _sut.AddPrescription(prescription));
+        Assert.Throws<ArgumentException>(() => sut.AddPrescription(prescription));
     }
 }
--- src/PrescriberPoint.Business/Prescriptions/PrescriptionService.cs
+++ src/PrescriberPoint.Business/Prescriptions/PrescriptionService.cs
@@ -1,15 +1,24 @@
-﻿using PrescriberPoint.Domain;
+﻿using PrescriberPoint.Business.Repositories;
+using PrescriberPoint.Domain;
 
 namespace PrescriberPoint.Business.Prescriptions;
 
 public class PrescriptionService : IPrescriptionService
 {
+    private IPrescriptionRepository _prescriptionRepository;
+
+    public PrescriptionService(IPrescriptionRepository prescriptionRepository)
+    {
+        _prescriptionRepository = prescriptionRepository;
+    }
+
     public void AddPrescription(Prescription prescription)
     {
         if (prescription == null || string.IsNullOrEmpty(prescription.Name) || string.IsNullOrEmpty(prescription.Description))
         {
             throw new ArgumentException("Prescription cannot contain empty data");
-
         }
+
+        _prescriptionRepository.Add(prescription);
     }
 }
