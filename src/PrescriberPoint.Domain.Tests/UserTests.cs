f9fa6c658036034088bc2b37ec5091ba1ef9d058
Alex Gonzalez <andritogv@gmail.com>
2023-10-03 23:04:09 -0600

    Added test for Id in User

--- src/PrescriberPoint.Domain.Tests/UserTests.cs
+++ src/PrescriberPoint.Domain.Tests/UserTests.cs
@@ -5,15 +5,18 @@ public class UserTests
     [Fact]
     public void TestGetSetProperties()
     {
+        const int id = 1;
         const string username = "testuser";
         const string password = "testpassword";
 
         var user = new User
         {
+            Id = id,
             Username = username,
             Password = password
         };
 
+        Assert.Equal(id, user.Id);
         Assert.Equal(username, user.Username);
         Assert.Equal(password, user.Password);
     }
